eb3b1c2f18f4f1fada9d2fe635a5f420319f9b95
romeo.hesch <romeo.hesch@gmail.com>
2018-06-06 15:45:27 +0200

    trying to get supertype done

--- server/src/domainObjects/SuperTypes/Contacto.js
+++ server/src/domainObjects/SuperTypes/Contacto.js
@@ -1,21 +1,62 @@
 export default class Contacto {
 
-  constructor(db,telefonos = [], emails = [], direcciones = []) {
+  constructor(db, tipo, transaction, telefonos = [], emails = []) {
     this.db = db;
-    this.telefonos = telefonos;
-    this.emails = emails;
-    this.direcciones = direcciones;
+    this.transaction = transaction;
+
+    this.contacto = null;
+    this.telefonos = null;
+    this.emails = null;
   }
 
-  creaTelefonos() {
-
+  async createContacto() {
+    let contacto;
+    try {
+      contacto = await this.db.create({ tipo }, {
+        transaction: transaction,
+      });
+      this.contacto = contacto.toJSON();
+    } catch (e) {
+      throw new Error('Error occured when creating Contacto');
+    }
   }
 
-  creaEmails() {
 
+  async createEmails(emails = []) {
+    try {
+      if (this.contacto === null) {
+        await createContacto();
+      }
+      //añadimos el id del contacto a los correos
+      emails.map(email => (
+        {
+          idContacto: this.contacto.id,
+        }
+      ));
+
+
+
+    } catch (e) {
+      throw new Error('Error occured in createEmails');
+    }
   }
-
-
 
 
 }
+
+/*
+ const correosArrObj = (emails) ? emails.map(email => ({
+  email,
+  idContacto: createdContacto.id,
+})) : null;
+
+const correosPromise = (correosArrObj === null) ? [] :
+  context.models.contactoCorreos.bulkCreate(
+    correosArrObj,
+    {
+      validate: true,
+      transaction: trans,
+      raw: true,
+    },
+  );
+  */
